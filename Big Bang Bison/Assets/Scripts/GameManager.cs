c1d437c4e4541cd1702530df4dc3563ff05e5abf
Samuel <samuelshin1997@gmail.com>
2020-01-30 23:47:30 -0800

    hills rotate activation based off time

--- Big Bang Bison/Assets/Scripts/GameManager.cs
+++ Big Bang Bison/Assets/Scripts/GameManager.cs
@@ -9,14 +9,18 @@ public class GameManager : MonoBehaviour
 {
     public Text text;
     public float gameTime_seconds; //in seconds
-    public float speed = 1;
+    public float speed = 0;
     public string winner;
     public GameObject hills;
+    public int hillFrequency_seconds; //in seconds
     Transform[] hillCollection;
     KeepScore keepScore;
     float timePassed_seconds; //in seconds;
-    public int hillFrequency_seconds; //in seconds
-    
+    bool hillChange = false;
+    Transform activeHill;
+    int index;
+    List<int> activatedHills = new List<int>();
+
     //Awake is always called before any Start functions
     void Awake()
     {
@@ -24,9 +28,17 @@ public class GameManager : MonoBehaviour
         keepScore = FindObjectOfType<KeepScore>();
         winner = "hi";
         hillCollection = hills.GetComponentsInChildren<Transform>();
-        timePassed_seconds = 0.0f;
-
+        timePassed_seconds = 1.0f;
+        
         hillCollection = ConstructHillArray(hillCollection);
+        foreach(Transform element in hillCollection)
+        {
+            element.gameObject.SetActive(false);
+        }
+        index = Random.Range(0, hillCollection.Length);
+        activeHill = hillCollection[index];
+        activeHill.gameObject.SetActive(true);
+        activatedHills.Add(index);
     }
 
     //Update is called every frame.
@@ -38,6 +50,7 @@ public class GameManager : MonoBehaviour
         {
             gameTime_seconds -= Time.deltaTime * speed;
             timePassed_seconds += Time.deltaTime * speed;
+            int timePassedTruncated = (int)(timePassed_seconds);
             string minutes = Mathf.Floor((gameTime_seconds % 3600) / 60).ToString("00");
             string seconds = (gameTime_seconds % 60).ToString("00");
             text.text = minutes + ":" + seconds;
@@ -49,7 +62,7 @@ public class GameManager : MonoBehaviour
                 SceneManager.LoadScene(2);
             }
 
-            HillChange(timePassed_seconds);
+            HillChange(timePassedTruncated);
         }
     }
 
@@ -87,6 +100,37 @@ public class GameManager : MonoBehaviour
 
     void HillChange(float timePassed)
     {
-        
+
+        if (timePassed == hillFrequency_seconds)
+        {
+            if (!hillChange)
+            {
+                Debug.Log("change hill");
+
+                activeHill.gameObject.SetActive(false);
+                activeHill = hillCollection[RandomSelectionExcept()];
+                activeHill.gameObject.SetActive(true);
+
+                hillChange = true;
+                timePassed_seconds = 0.0f;
+            }
+        }
+        else
+        {
+            hillChange = false;
+        }
+    }
+
+    int RandomSelectionExcept()
+    {
+        int rng = -1;
+        do
+        {
+            rng = Random.Range(0, hillCollection.Length);
+        }
+        while (activatedHills.Contains(rng));
+
+        activatedHills.Add(rng);
+        return rng;
     }
 }
