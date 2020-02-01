ab5bc6ad8b3c51d0761050a55f0e1fcaacbfa3a6
madmaksman <maxcburkhart@gmail.com>
2020-01-31 17:36:53 -0800

    Added Vertical Cam
    Added vertical Cam and fixed a rotation bug

--- Big Bang Bison/Assets/ThirdPersonController.cs
+++ Big Bang Bison/Assets/ThirdPersonController.cs
@@ -8,9 +8,6 @@ public class ThirdPersonController : MonoBehaviour
     public float Speed = 10.0f;
     public float rotationSpeed = 100.0f;
     public int PlayerNum;
-    //public string Horizontal;
-    //public string Vertical;
-    //public string MouseX;
 
 
     // Update is called once per frame
@@ -23,51 +20,31 @@ public class ThirdPersonController : MonoBehaviour
         
 
         if (PlayerNum == 1){
-            float translationX = Input.GetAxis("Vertical1") * Speed;
-            float translationY = Input.GetAxis("Horizontal1") * Speed;
-
-            float rotation = Input.GetAxis("MouseX1") * rotationSpeed;
-            translationX *= Time.deltaTime;
-            translationY *= Time.deltaTime;
-            rotation *= Time.deltaTime;
-            transform.Translate(translationY, 0, translationX);
-            transform.Rotate(0, rotation, 0);
+            float translationX = Input.GetAxis("Vertical1");
+            float translationY = Input.GetAxis("Horizontal1");
+            Vector3 playerMovment = new Vector3(translationY, 0f, translationX) * Speed *Time.deltaTime;
+            transform.Translate(playerMovment, Space.Self);
         }
         else if (PlayerNum == 2)
         {
-            float translationX = Input.GetAxis("Vertical2") * Speed;
-            float translationY = Input.GetAxis("Horizontal2") * Speed;
-
-            float rotation = Input.GetAxis("MouseX2") * rotationSpeed;
-            translationX *= Time.deltaTime;
-            translationY *= Time.deltaTime;
-            rotation *= Time.deltaTime;
-            transform.Translate(translationY, 0, translationX);
-            transform.Rotate(0, rotation, 0);
+            float translationX = Input.GetAxis("Vertical2");
+            float translationY = Input.GetAxis("Horizontal2");
+            Vector3 playerMovment = new Vector3(translationY, 0f, translationX) * Speed * Time.deltaTime;
+            transform.Translate(playerMovment, Space.Self);
         }
         else if (PlayerNum == 3)
         {
-            float translationX = Input.GetAxis("Vertical3") * Speed;
-            float translationY = Input.GetAxis("Horizontal3") * Speed;
-
-            float rotation = Input.GetAxis("MouseX3") * rotationSpeed;
-            translationX *= Time.deltaTime;
-            translationY *= Time.deltaTime;
-            rotation *= Time.deltaTime;
-            transform.Translate(translationY, 0, translationX);
-            transform.Rotate(0, rotation, 0);
+            float translationX = Input.GetAxis("Vertical3");
+            float translationY = Input.GetAxis("Horizontal3");
+            Vector3 playerMovment = new Vector3(translationY, 0f, translationX) * Speed * Time.deltaTime;
+            transform.Translate(playerMovment, Space.Self);
         }
         else if (PlayerNum == 4)
         {
-            float translationX = Input.GetAxis("Vertical4") * Speed;
-            float translationY = Input.GetAxis("Horizontal4") * Speed;
-
-            float rotation = Input.GetAxis("MouseX4") * rotationSpeed;
-            translationX *= Time.deltaTime;
-            translationY *= Time.deltaTime;
-            rotation *= Time.deltaTime;
-            transform.Translate(translationY, 0, translationX);
-            transform.Rotate(0, rotation, 0);
+            float translationX = Input.GetAxis("Vertical4");
+            float translationY = Input.GetAxis("Horizontal4");
+            Vector3 playerMovment = new Vector3(translationY, 0f, translationX) * Speed * Time.deltaTime;
+            transform.Translate(playerMovment, Space.Self);
         }
 
 
